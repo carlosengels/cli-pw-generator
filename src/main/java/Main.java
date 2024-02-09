2da04f5d90a90b9a8ce42ac0a38e00aff26ff00e
CJE <engels.carlos@gmail.com>
2024-02-09 07:56:49 -0700

    started work on flags

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,32 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        //TODO add options to use flags to set password length and limit to user no symbols and/or no digis
+        String allLetters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        String digits = "0123456789";
+        String specialChars = "!@#$%^&*()_-+=<>?";
+        String allAvailableChars = allLetters + digits + specialChars;
+        String charPool = allAvailableChars;
+        int defaultPasswordLength = 15;
+        StringBuilder randomPassword = new StringBuilder();
+
+        //TODO add options to use flags to set password length and limit to user no symbols and/or no digits
+        // switch cases for different args
+        // -l## (defined length)
+        // -c  (no specials char)
+        // -d (no digits)
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-c":
+                    charPool = allLetters + digits;
+                    break;
+                case "-d":
+                    charPool = allLetters + specialChars;
+                    break;
+                default:
+                    charPool = allAvailableChars;
+            }
+        }
 
         //test
         if (args.length != 0) {
@@ -11,16 +36,11 @@ public class Main {
         }
 
 
-
-        String allAvailableChars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%^&*()_-+=<>?";
-        int passwordLength = 15;
-        StringBuilder randomPassword = new StringBuilder();
-
         System.out.println("Starting random pw generator");
 
-        while (randomPassword.length() < passwordLength) {
+        while (randomPassword.length() < defaultPasswordLength) {
             Random random = new Random();
-            randomPassword.append(allAvailableChars.charAt(random.nextInt(allAvailableChars.length())));
+            randomPassword.append(charPool.charAt(random.nextInt(charPool.length())));
         }
 
         System.out.println(randomPassword);
